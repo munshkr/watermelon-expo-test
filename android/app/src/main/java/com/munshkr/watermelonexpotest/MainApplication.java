8a5d28768f2ed23a855adb1050be2c47e3744087
Damián Silvani <munshkr@gmail.com>
2023-01-07 11:36:20 -0300

    chore: Configure Android for WatermelonDB
    Differences from WatermelonDB instructions:
    * Use Kotlin >=1.6.0

--- android/app/src/main/java/com/munshkr/watermelonexpotest/MainApplication.java
+++ android/app/src/main/java/com/munshkr/watermelonexpotest/MainApplication.java
@@ -20,6 +20,13 @@ import expo.modules.ReactNativeHostWrapper;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
+import java.util.Arrays;
+import com.facebook.react.bridge.JSIModuleSpec;
+import com.facebook.react.bridge.JSIModulePackage;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.JavaScriptContextHolder;
+import com.nozbe.watermelondb.jsi.WatermelonDBJSIPackage;
+
 public class MainApplication extends Application implements ReactApplication {
   private final ReactNativeHost mReactNativeHost = new ReactNativeHostWrapper(
     this,
@@ -41,6 +48,24 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected String getJSMainModuleName() {
       return "index";
+    }
+
+    @Override
+    protected JSIModulePackage getJSIModulePackage() {
+      return new JSIModulePackage() {
+        @Override
+        public List<JSIModuleSpec> getJSIModules(
+          final ReactApplicationContext reactApplicationContext,
+          final JavaScriptContextHolder jsContext
+        ) {
+          List<JSIModuleSpec> modules = Arrays.asList();
+
+          modules.addAll(new WatermelonDBJSIPackage().getJSIModules(reactApplicationContext, jsContext));
+          // Add more JSI packages here by conventions above...
+
+          return modules;
+        }
+      };
     }
   });
 
